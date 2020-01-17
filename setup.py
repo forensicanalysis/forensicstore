8bdcfbff98fbb9d2aa5f0341713ff29430823637
Jonas Plum <jonas.plum@siemens.com>
2020-01-17 15:13:12 +0100

    Setup release workflows

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='forensicstore',
-    version='0.12.0',
+    version='0.12.2',
     url='https://github.com/forensicanalysis/forensicstore',
     author='Jonas Plum',
     author_email='jonas.plum@siemens.com',
