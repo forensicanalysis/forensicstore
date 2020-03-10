b2789f43dcdc0e3520148b802205fa83cce68f7d
Jonas Plum <jonas.plum@siemens.com>
2020-03-10 12:26:44 +0100

    Change pyforensicstore parameter

--- pyforensicstore/cmd/__main__.py
+++ pyforensicstore/cmd/__main__.py
@@ -20,44 +20,20 @@
 # Author(s): Jonas Plum
 
 import argparse
+import forensicstore
 import json
 import sys
 
-import forensicstore
 
 def main():
     root_parser = argparse.ArgumentParser(description='Handle forensicstores')
     root_subparsers = root_parser.add_subparsers(dest='root_command')
     root_subparsers.required = True
-    parser = root_subparsers.add_parser("item")
-
-    subparsers = parser.add_subparsers(dest='command')
-    subparsers.required = True
 
     create_parser = root_subparsers.add_parser("create")
     create_parser.add_argument('store')
 
-    get_parser = subparsers.add_parser("get")
-    get_parser.add_argument('id')
-    get_parser.add_argument('store')
-
-    select_parser = subparsers.add_parser("select")
-    select_parser.add_argument('type')
-    select_parser.add_argument('store')
-
-    all_parser = subparsers.add_parser("all")
-    all_parser.add_argument('store')
-
-    insert_parser = subparsers.add_parser("insert")
-    insert_parser.add_argument('json')
-    insert_parser.add_argument('store')
-
-    update_parser = subparsers.add_parser("update")
-    update_parser.add_argument('id')
-    update_parser.add_argument('json')
-    update_parser.add_argument('store')
-
-    import_parser = subparsers.add_parser("import")
+    import_parser = root_subparsers.add_parser("import")
     import_parser.add_argument('url')
     import_parser.add_argument('store')
 
@@ -65,42 +41,36 @@ def main():
     validate_parser.add_argument('store')
     validate_parser.add_argument('--no-fail', action='store_true', dest="nofail")
 
+    item_parser = root_subparsers.add_parser("item")
+    item_subparsers = item_parser.add_subparsers(dest='command')
+    item_subparsers.required = True
+
+    get_parser = item_subparsers.add_parser("get")
+    get_parser.add_argument('id')
+    get_parser.add_argument('store')
+
+    select_parser = item_subparsers.add_parser("select")
+    select_parser.add_argument('type')
+    select_parser.add_argument('store')
+
+    all_parser = item_subparsers.add_parser("all")
+    all_parser.add_argument('store')
+
+    insert_parser = item_subparsers.add_parser("insert")
+    insert_parser.add_argument('json')
+    insert_parser.add_argument('store')
+
+    update_parser = item_subparsers.add_parser("update")
+    update_parser.add_argument('id')
+    update_parser.add_argument('json')
+    update_parser.add_argument('store')
+
     args = root_parser.parse_args()
 
-    if args.command == "create":
+    if args.root_command == "create":
         store = forensicstore.connect(args.store)
         store.close()
-    elif args.command == "get":
-        store = forensicstore.connect(args.store)
-        item = store.get(args.id)
-        print(json.dumps(item))
-        store.close()
-    elif args.command == "select":
-        store = forensicstore.connect(args.store)
-        items = list(store.select(args.type))
-        print(json.dumps(items))
-        store.close()
-    elif args.command == "all":
-        store = forensicstore.connect(args.store)
-        items = list(store.all())
-        print(json.dumps(items))
-        store.close()
-    elif args.command == "insert":
-        store = forensicstore.connect(args.store)
-        item = store.insert(json.loads(args.json))
-        print(json.dumps(item))
-        store.close()
-    elif args.command == "update":
-        store = forensicstore.connect(args.store)
-        print(args.json)
-        item = store.update(args.id, json.loads(args.json))
-        print(json.dumps(item))
-        store.close()
-    elif args.command == "import":
-        store = forensicstore.connect(args.store)
-        store.import_forensicstore(args.url)
-        store.close()
-    elif args.command == "validate":
+    elif args.root_command == "validate":
         store = forensicstore.connect(args.store)
         errors = store.validate()
         if errors:
@@ -108,8 +78,41 @@ def main():
         if args.nofail:
             sys.exit(0)
         sys.exit(len(errors))
+    elif args.root_command == "import":
+        store = forensicstore.connect(args.store)
+        store.import_forensicstore(args.url)
+        store.close()
+    elif args.root_command == "item":
+        if args.command == "get":
+            store = forensicstore.connect(args.store)
+            item = store.get(args.id)
+            print(json.dumps(item))
+            store.close()
+        elif args.command == "select":
+            store = forensicstore.connect(args.store)
+            items = list(store.select(args.type))
+            print(json.dumps(items))
+            store.close()
+        elif args.command == "all":
+            store = forensicstore.connect(args.store)
+            items = list(store.all())
+            print(json.dumps(items))
+            store.close()
+        elif args.command == "insert":
+            store = forensicstore.connect(args.store)
+            item = store.insert(json.loads(args.json))
+            print(json.dumps(item))
+            store.close()
+        elif args.command == "update":
+            store = forensicstore.connect(args.store)
+            print(args.json)
+            item = store.update(args.id, json.loads(args.json))
+            print(json.dumps(item))
+            store.close()
+        else:
+            NotImplementedError("Sub command %s does not exist" % args.command)
     else:
-        NotImplementedError("Command %s does not exist" % args.command)
+        NotImplementedError("Command %s does not exist" % args.root_command)
 
 
 if __name__ == '__main__':
